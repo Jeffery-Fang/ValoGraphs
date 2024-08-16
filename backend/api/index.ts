88644fd4e08153a9d36ca71b12b170151c9a5db5
Jeffery Fang <Jefferyfang324@gmail.com>
2024-08-16 16:05:34 -0400

    modified index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,31 +1,3 @@
-import 'dotenv/config'
 import app from '../src/app.js'
-import { AppDataSource } from '../src/data-source.js'
 
-const PORT: number = Number(process.env.PORT) || 3000
-
-AppDataSource.initialize()
-    .then((): void => {
-        console.log('Database connection initialized')
-
-        app.listen(PORT, (): void => {
-            console.log(`server started on port ${PORT}`)
-        })
-    })
-    .catch((err): void => {
-        console.log('Error connecting to the database', err)
-    })
-
-// process.on('SIGINT', (): void => {
-//     AppDataSource.destroy()
-//         .then((): void => {
-//             console.log('Database connection closed')
-//             process.exit(0)
-//         })
-//         .catch((err): void => {
-//             console.log('Error closing connection to database', err)
-//             process.exit(1)
-//         })
-// })
-
-module.exports = app
+export default app
